0036c7237b275bb8ee9c1cff50c1e3386f9c5ecc
prashantsingh480 <47595554+prashantsingh480@users.noreply.github.com>
2020-08-27 02:45:32 +0530

    Update new.php

--- new.php
+++ new.php
@@ -114,6 +114,7 @@
 
                     <?php
                     // login checking.
+                    session_start();
                     ini_set('session.save_handler', 'memcached');
                     ini_set('session.save_path', getenv('mc4.dev.ec2.memcachier.com'));
                     if (version_compare(phpversion('memcached'), '3', '>=')) {
